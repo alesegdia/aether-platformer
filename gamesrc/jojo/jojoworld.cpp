e645b1828427e621d65d227d4bf48d6641e12746
Alejandro Seguí <alesegdia@gmail.com>
2025-02-16 23:03:55 +0100

    top down scroller working

--- gamesrc/jojo/jojoworld.cpp
+++ gamesrc/jojo/jojoworld.cpp
@@ -49,7 +49,7 @@ namespace jojo {
 				float(JojoConfig::instance().windowWidth),
 				float(JojoConfig::instance().windowHeight) };
 		auto cam = aether::GEngine->GetCamera(aether::render::CameraFlags::Default);
-		cam->SetOrthographicSize(5.f);
+		cam->SetOrthographicSize(10.f);
 		cam->SetPosition(100.f, 100.f, -100.f);
 
 		m_platformerScroll = std::make_shared<aether::render::PlatformerScroller>(
