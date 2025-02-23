13e730e3f958f5856155caac69c1309492d24140
Alejandro Seguí <alesegdia@gmail.com>
2025-02-23 18:18:31 +0100

    fixes

--- assets/jojo/boot.lua
+++ assets/jojo/boot.lua
@@ -1,16 +1,16 @@
 startingMap = "assets/jojo/levels/level2.tmx"
 
-walkSpeed = 2.0
+walkSpeed = 1.0
 walkSpeedIncrement = 0.05
 walkFriction = 0.95
 
-dashSpeed = 5.0
+dashSpeed = 3.0
 dashFriction = 0.95
 
 jumpForce = 3.0
 gravityFactor = 0.1
 fallingCap = 3.0
 
-dashDuration = 5.0
+dashDuration = 0.2
 dashCooldown = 60
 
