31453aeb453ba9813f84bdcefee76a0ffa730369
Alejandro Seguí <alesegdia@gmail.com>
2025-02-28 16:47:30 +0100

    enerjim assets

---
+++ assets/enerjim/tileset.tsx
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="tileset.png" width="256" height="256"/>
+ <wangsets>
+  <wangset name="WalkableAreas" type="corner" tile="-1">
+   <wangcolor name="Ground" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="Air" color="#18d8ff" tile="-1" probability="1"/>
+   <wangcolor name="BackWall" color="#0000ff" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="1" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="2" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="3" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="4" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="6" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="7" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="8" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="9" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="10" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="11" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="12" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="13" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="14" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="16" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="17" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="18" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="19" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="20" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="21" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="22" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="24" wangid="0,2,0,3,0,2,0,2"/>
+   <wangtile tileid="25" wangid="0,2,0,3,0,3,0,2"/>
+   <wangtile tileid="26" wangid="0,2,0,2,0,3,0,2"/>
+   <wangtile tileid="27" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="32" wangid="0,3,0,3,0,2,0,2"/>
+   <wangtile tileid="33" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="34" wangid="0,2,0,2,0,3,0,3"/>
+   <wangtile tileid="35" wangid="0,3,0,3,0,3,0,2"/>
+   <wangtile tileid="36" wangid="0,2,0,3,0,3,0,3"/>
+   <wangtile tileid="40" wangid="0,3,0,2,0,2,0,2"/>
+   <wangtile tileid="41" wangid="0,3,0,2,0,2,0,3"/>
+   <wangtile tileid="42" wangid="0,2,0,2,0,2,0,3"/>
+   <wangtile tileid="43" wangid="0,3,0,3,0,2,0,3"/>
+   <wangtile tileid="44" wangid="0,3,0,2,0,3,0,3"/>
+  </wangset>
+ </wangsets>
+</tileset>
